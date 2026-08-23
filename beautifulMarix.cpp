15d4967dfdc36d863ccae26efee489adaa6e9013
yeswanth <yeswanthreddy2801@gmail.com>
2026-08-23 22:18:57 +0530

    stone on the table 266A

--- beautifulMarix.cpp
+++ beautifulMarix.cpp
@@ -4,13 +4,22 @@ int main()
 {
     int n=5;
     vector<vector<int>>v(n,vector<int>(n));
+    int ind;
+    int jnd;
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
         {
             cin>>v[i][j];
+            if(v[i][j]==1)
+            {
+                ind=i;
+                jnd=j;
+            }
             
         }
     }
+    cout<<abs(ind-2)+abs(jnd-2);
+
 
 }
