948db932e1c9568284d962a57cb0862eca3743f1
yeswanth <yeswanthreddy2801@gmail.com>
2026-08-24 14:15:32 +0530

    word upper lower 59A

---
+++ word.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int n=s.length();
+    int upper=0,lower=0;
+    for(int i=0;i<n;i++)
+    {
+        if(s[i]>='A' && s[i]<='Z')
+        {
+            upper++;
+        }
+        else lower++;
+    }
+    if(upper>lower)
+    {
+        transform(s.begin(),s.end(),s.begin(),::toupper);
+    }
+else{
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+
+    }
+    cout<<s;
+
+}
