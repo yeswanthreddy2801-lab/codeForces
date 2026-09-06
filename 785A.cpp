61b8e0ebf2af6b898c2a12e3a95c7afc7589b327
yeswanth <yeswanthreddy2801@gmail.com>
2026-09-06 11:05:31 +0530

    add polyhedra 785A

---
+++ 785A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int ans=0;
+    for(int i=0;i<n;i++)
+    {
+        string s;
+        cin>>s;
+        if(s=="Tetrahedron")
+        ans+=4;
+        else if(s=="Cube")
+        ans+=6;
+        else if(s=="Octahedron")
+        ans+=8;
+        else if(s=="Dodecahedron")
+        ans+=12;
+        else ans+=20;
+    }
+    cout<<ans;
+}
