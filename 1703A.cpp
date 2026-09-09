a37dadafe51e0ea7d0bf7701c6024f4a206a065a
yeswanth <yeswanthreddy2801@gmail.com>
2026-09-09 21:45:21 +0530

    add yes or yes 1703A

---
+++ 1703A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<string>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        string x=v[i];
+        transform(x.begin(),x.end(),x.begin(),::tolower);
+        if(x=="yes")
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        cout<<"NO"<<endl;
+    }
+}
